34b774b6e1995e7fc8455f116a7d6dc7b7ae0ab5
Muhammed <faheem@metrictreelabs.com>
2022-08-28 15:35:57 +0530

    first_commit

---
+++ build/precache-manifest.f81a10aef39abe6ff82d53491b5cd326.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d426fb4da5f8fa8bcd82021884a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d890235c1d65db81e4",
+    "url": "/static/js/2.6c0478f2.chunk.js"
+  },
+  {
+    "revision": "f24b473656bdcdf7a40306c1c14414ef",
+    "url": "/static/js/2.6c0478f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c7c6863dbd8e044cf4",
+    "url": "/static/js/main.763929af.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "71c818c4b163ca2988d833c2a52a3fd2",
+    "url": "/static/media/micmute.71c818c4.svg"
+  },
+  {
+    "revision": "d15ab7f56c030ba8f78389f88f17c28e",
+    "url": "/static/media/micunmute.d15ab7f5.svg"
+  },
+  {
+    "revision": "9f0afb70cf64fc56e00c253be2a88889",
+    "url": "/static/media/webcam.9f0afb70.svg"
+  },
+  {
+    "revision": "f87f687925585259389f10d0c48c8127",
+    "url": "/static/media/webcamoff.f87f6879.svg"
+  }
+]);
